e86be5c7f14cada2946d7e915284693e3ea38f07
Dennis Kaselow <dennis.kaselow+github@gmail.com>
2014-03-04 19:00:25 +0100

    initial setup

---
+++ lib/client.dart
@@ -0,0 +1,15 @@
+library client;
+
+import 'dart:html';
+export 'dart:html';
+
+import 'package:3hgj_witch/shared.dart';
+export 'package:3hgj_witch/shared.dart';
+
+import 'package:canvas_query/canvas_query.dart';
+export 'package:canvas_query/canvas_query.dart';
+import 'package:gamedev_helpers/gamedev_helpers.dart';
+export 'package:gamedev_helpers/gamedev_helpers.dart';
+
+//part 'src/client/systems/name.dart';
+part 'src/client/systems/rendering.dart';
---
+++ web/3hgj_witch.dart
@@ -0,0 +1,32 @@
+import 'package:3hgj_witch/client.dart';
+
+@MirrorsUsed(targets: const [
+                            ])
+import 'dart:mirrors';
+
+void main() {
+  new Game().start();
+}
+
+class Game extends GameBase {
+
+  Game() : super.noAssets('3hgj_witch', 'canvas', 800, 600);
+
+  void createEntities() {
+    // addEntity([Component1, Component2]);
+  }
+
+  List<EntitySystem> getSystems() {
+    return [
+            new CanvasCleaningSystem(canvas),
+            new FpsRenderingSystem(ctx)
+    ];
+  }
+
+  Future onInit() {
+  }
+
+  Future onInitDone() {
+  }
+}
+
